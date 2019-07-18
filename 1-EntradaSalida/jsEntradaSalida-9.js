e857683eef4c093a0a5914f2f9b0c62010c42502
eugeniaalegre <52534699+eugeniaalegre@users.noreply.github.com>
2019-07-18 09:00:01 -0300

    UWUWU

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+  var sueldo
+  var Resultado
 
-	
+  sueldo = document.getElementById("sueldo").value;
+  sueldo = parseInt(sueldo)
+
+  resultado = document.getElementById("Resultado").value;
+
+
+
+
+
+
+
+
 }
